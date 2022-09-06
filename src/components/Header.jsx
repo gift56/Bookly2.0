17479ab70fa47b14dccddbcaa2fb11abdd13ae6d
gift56 <efeasiughu@gmail.com>
2022-09-06 17:15:29 +0100

    added some stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,17 @@ import Logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header>
-      <div className="contain">
+      <nav className="contain">
         <div className="logo ">
           <img src={Logo} alt="logo" />
         </div>
-      </div>
+
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+      </nav>
     </header>
   );
 };
