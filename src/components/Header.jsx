2dc65d0acfbacfc0af6edbcfa8b251f8fd353d0e
gift56 <efeasiughu@gmail.com>
2022-09-06 17:09:20 +0100

    sass file successfully worked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <div className="logo ">
         <img src={Logo} alt="logo" />
       </div>
     </header>
