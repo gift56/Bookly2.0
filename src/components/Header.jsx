d086f828766d7846f05d31087d3292f8cc18a621
gift56 <efeasiughu@gmail.com>
2022-09-07 06:09:17 +0100

    almost fixingissues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
 import cart from "../assets/cart.png";
 import Button from "./Button";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
     <header>
       <nav className="contain desktopNav">
@@ -11,6 +13,23 @@ const Header = () => {
           <img src={Logo} alt="logo" />
         </div>
 
+        <ul className="ul">
+          <li>Home</li>
+          <li>Categories</li>
+          <li>Request a book</li>
+          <li>
+            Cart <img src={cart} alt="/" />
+          </li>
+          <Button text="Login" className="loginbtn" />
+        </ul>
+        <div
+          onClick={() => setShow((prev) => !prev)}
+          className="toggle"
+        >
+          {show ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+      </nav>
+      <nav className={`mobileNav`}>
         <ul className="ul">
           <li>Home</li>
           <li>Categories</li>
