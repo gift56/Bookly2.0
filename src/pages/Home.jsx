f5433a9fcc776e704dab921b02c383c1fb0a6c0f
gift56 <efeasiughu@gmail.com>
2022-09-07 11:15:23 +0100

    working on hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Welcome from "../components/Welcome";
 import "../sass/Header.scss";
-
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Welcome />
     </>
   );
 };
