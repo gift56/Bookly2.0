8828359b56dd15d9fec9370a7e2d0de5f79a24f2
gift56 <efeasiughu@gmail.com>
2022-09-09 15:22:40 +0100

    created physics area

--- src/utils/booksData.js
+++ src/utils/booksData.js
@@ -1,7 +1,28 @@
 import englishBook from "../assets/englishB.png";
+import mathsBook from "../assets/mathB.png";
+import physicsBook from "../assets/physicsB.png";
+import governBook from "../assets/governB.png";
+import historyBook from "../assets/historyB.png";
 
 export const featuresData = [
   {
     title: "Top Features",
+    books: [
+      {
+        img: mathsBook,
+        bookText: "New General Maths 1",
+        price: "3000",
+      },
+      {
+        img: englishBook,
+        bookText: "Nigeria Pry English",
+        price: "3000",
+      },
+      {
+        img: physicsBook,
+        bookText: "Exam Focus Physics",
+        price: "3000",
+      },
+    ],
   },
 ];
