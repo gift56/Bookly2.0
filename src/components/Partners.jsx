2bfa20de3d6bbd43a7e5b6e2dc7a06431f4161f0
gift56 <efeasiughu@gmail.com>
2022-09-10 11:43:31 +0100

    still working on partners area

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <section className='partners'>
+        <div className="contain">
+            <div className="partnersCon">
+                <h2>Partners</h2>
+                <div className="clientsCon">
+
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
