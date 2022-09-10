15f7d58fecc2af0bbeb526db50d847116a3a55fb
gift56 <efeasiughu@gmail.com>
2022-09-10 12:08:31 +0100

    working on styles

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import Jumia from '../assets/jumia.svg';
-import Jiji from '../assets/jiji.svg';
-import Amazon from '../assets/amazon.svg';
-import Shopify from '../assets/shopify.svg';
-import Shoprite from '../assets/shoprite.svg';
-import Justrite from '../assets/justrite.svg';
+import React from "react";
+import Jumia from "../assets/jumia.svg";
+import Jiji from "../assets/jiji.svg";
+import Amazon from "../assets/amazon.svg";
+import Shopify from "../assets/shopify.svg";
+import Shoprite from "../assets/shoprite.svg";
+import Justrite from "../assets/justrite.svg";
 
 const Partners = () => {
+  const clientsImages = [
+    Jumia,
+    Jiji,
+    Amazon,
+    Shopify,
+    Shoprite,
+    Justrite,
+  ];
   return (
-    <section className='partners'>
-        <div className="contain">
-            <div className="partnersCon">
-                <h2>Partners</h2>
-                <div className="clientsCon">
-
-                </div>
-            </div>
+    <section className="partners">
+      <div className="contain">
+        <div className="partnersCon">
+          <h2>Partners</h2>
+          <div className="clientsCon">
+            {clientsImages.map((client, index) => (
+              <div key={index} className="images">
+                <img src={client} alt="/" />
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
