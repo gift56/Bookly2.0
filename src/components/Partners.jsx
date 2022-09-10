ff8a6920101dd45d0775ca4c3d2b8e69aca7b26e
gift56 <efeasiughu@gmail.com>
2022-09-10 11:50:17 +0100

    import img

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Jumia from '../assets/jumia.svg';
+import Jiji from '../assets/jiji.svg';
+import Amazon from '../assets/amazon.svg';
+import Shopify from '../assets/shopify.svg';
+import Shoprite from '../assets/shoprite.svg';
+import Justrite from '../assets/justrite.svg';
 
 const Partners = () => {
   return (
