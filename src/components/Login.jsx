097d1179cddc43252329cf59b35ae829ac66a1b0
gift56 <efeasiughu@gmail.com>
2022-09-12 14:11:01 +0100

    added eye logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "../images/loginImg.svg";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { CgCloseO } from "react-icons/cg";
 import { BsEyeSlash, BsEye } from "react-icons/bs";
 
 const Login = () => {
+  const [eye, setEye] = useState(false);
+
+  const showPassword = () => {
+    setEye(!eye);
+  };
   return (
     <div>
       <div className="contain">
@@ -20,7 +25,25 @@ const Login = () => {
             <p>Hi User, Welcome back to Bookly! We missed you</p>
           </div>
           <div className="formContainer">
-            <div className="leftSide"></div>
+            <div className="leftSide">
+              <form className="myForm">
+                <div className="formControl">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" />
+                </div>
+                <div className="formControl">
+                  <label htmlFor="password">Password</label>
+                  <input type={!eye ? "password" : "text"} />
+                  <div className="show">
+                    {!eye ? (
+                      <BsEyeSlash onClick={showPassword} />
+                    ) : (
+                      <BsEye onClick={showPassword} />
+                    )}
+                  </div>
+                </div>
+              </form>
+            </div>
             <div className="rightSide">
               <img src={Image} alt="/" />
             </div>
