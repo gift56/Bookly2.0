d4c4e785dfc90502b443abdb17a524588c5f5a7f
gift56 <efeasiughu@gmail.com>
2022-09-12 15:59:51 +0100

    adding signup

--- src/Routes/Pages.jsx
+++ src/Routes/Pages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Forms from "../pages/Forms";
+import SignUp from "../pages/SignUp";
 
 const Pages = () => {
   return (
@@ -9,6 +10,7 @@ const Pages = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Forms />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
