cb1701c46f7e21fb9ed8d0b6de6b920aa61b3488
gift56 <efeasiughu@gmail.com>
2022-09-12 13:15:33 +0100

    working on login area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Forms from "./pages/Forms";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Forms />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Login from "../components/Login";
 
 const Forms = () => {
   return (
     <div>
-      
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default Forms
+export default Forms;
