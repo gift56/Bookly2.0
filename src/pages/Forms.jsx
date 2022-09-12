5c590f4e207114190639f888535d87986b822ba5
gift56 <efeasiughu@gmail.com>
2022-09-12 15:52:08 +0100

    added some new stuff

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 import Login from "../components/Login";
 import "../sass/form.scss";
 
 const Forms = () => {
   return (
     <div>
+      <Header/>
       <Login />
     </div>
   );
