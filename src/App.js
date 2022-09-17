1ce67e5466543232336d9821f925ef2046fdefba
gift56 <efeasiughu@gmail.com>
2022-09-17 11:18:18 +0100

    wrapped app with authcontext provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { AuthContextProvider } from "./contexts/authContext";
 import Pages from "./Routes/Pages";
 
 const App = () => {
   return (
     <div>
-      <Pages />
+      <AuthContextProvider>
+        <Pages />
+      </AuthContextProvider>
     </div>
   );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,11 +8,16 @@ import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import "../sass/form.scss";
+import { UserAuth } from "../context/AuthContext";
 
 const SignUp = () => {
   const [eye, setEye] = useState(false);
   const [confirmEye, setConfirmEye] = useState(false);
+
+
+
   const footerLinks = [<FaGoogle />, <FaApple />, <FaFacebookF />];
+
 
   const showPassword = () => {
     setEye(!eye);
