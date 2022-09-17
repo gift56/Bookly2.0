09277ef07749a7b7f9f7eef0e630489b5bf012b4
gift56 <efeasiughu@gmail.com>
2022-09-17 09:47:40 +0100

    working on authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkdkeGN5Td1zd6iSHFrmlbgX9P-dlVUEs",
-  authDomain: "bookly-auth-75480.firebaseapp.com",
-  projectId: "bookly-auth-75480",
-  storageBucket: "bookly-auth-75480.appspot.com",
-  messagingSenderId: "557072692918",
-  appId: "1:557072692918:web:1109473024fb76ab795817"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
