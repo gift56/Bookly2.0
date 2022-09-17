9af67757e17f4af3a074efa3c912025b08a657a6
gift56 <efeasiughu@gmail.com>
2022-09-17 11:53:52 +0100

    successfull added some updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import AuthContextProvider from "./context/AuthContext";
 import Pages from "./Routes/Pages";
 
 const App = () => {
   return (
     <>
-      <Pages />
+      <AuthContextProvider>
+        <Pages />
+      </AuthContextProvider>
     </>
   );
 };
