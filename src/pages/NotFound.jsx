944ac5bc13458d7e1e54ff97baf76fbfa391392e
gift56 <efeasiughu@gmail.com>
2022-09-20 12:29:49 +0100

    done with 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BiError } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div>
+      <Header btnText="login" linkText="login" />
+      <div className="pageNotfound">
+        <BiError size={90} color="red" />
+        <h1>Page Not Found</h1>
+        <span>
+          Go back <Link to="/">home</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
