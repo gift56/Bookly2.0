7be6e138778809b35cc8e1970ab431b486b56a86
gift56 <efeasiughu@gmail.com>
2022-10-25 11:23:13 +0100

    working on index.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import Pages from "./Routes/Pages";
+import './sass/index.scss';
 
 const App = () => {
   return (
