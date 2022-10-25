6d27bedfdafe0e87feaec8fb8b1dbe41c9ca05a4
gift56 <efeasiughu@gmail.com>
2022-10-25 14:49:08 +0100

    updating header components

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -9,66 +9,7 @@ const AccountPage = () => {
 
   return (
     <>
-      <header id="Home" className="accHead">
-        <nav className="contain desktopNav">
-          <div className="logo">
-            <Link to="/">
-              <img src={Logo} alt="logo" />
-            </Link>
-          </div>
-
-          <ul className="ul">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/catergories">Categories</Link>
-            </li>
-            <li>
-              <Link to="/request">Request a book</Link>
-            </li>
-            <li>
-              <Link to="/cart">
-                Cart <img src={cart} alt="/" />
-              </Link>
-            </li>
-            <Button text="My Account" className="loginbtn" />
-          </ul>
-          <div onClick={() => setShow((prev) => !prev)} className="toggle">
-            {show ? <FaTimes size={30} /> : <FaBars size={30} />}
-          </div>
-        </nav>
-        <nav className={`mobileNav ${show ? "normal" : "left"}`}>
-          <ul className="ul">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/catergories">Categories</Link>
-            </li>
-            <li>
-              <Link to="/request">Request a book</Link>
-            </li>
-            <li>
-              <Link to="/cart">
-                Cart <img src={cart} alt="/" />
-              </Link>
-            </li>
-            <Button text="My Account" className="loginbtn" />
-          </ul>
-        </nav>
-        <div className="dropdown">
-          <ul className="dropdownList">
-            <Button text="Log Out" className="loginbtn" />
-            {AccountLinks.map(({ img, name }, index) => (
-              <li key={index}>
-                <img src="" alt="" />
-                <p>{name}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </header>
+      
     </>
   );
 };
