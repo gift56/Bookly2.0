d9e53ea0c251762d9bb7da66572960e274c1f0af
gift56 <efeasiughu@gmail.com>
2022-10-25 11:54:54 +0100

    adding new validations

--- src/components/FormData/Validation.js
+++ src/components/FormData/Validation.js
@@ -2,6 +2,12 @@ export const Validate = (values) => {
   let errors = {};
   let patten = /\S+@\S+\.\S+/;
 
+  if (!values.name) {
+    errors.name = "Kindly Enter your FullName";
+  } else if (values.name.length < 4) {
+    errors.name = "FullName should be longer than 4 characters";
+  }
+
   if (!values.email) {
     errors.email = "Email Field is Required";
   } else if (!patten.test(values.email)) {
