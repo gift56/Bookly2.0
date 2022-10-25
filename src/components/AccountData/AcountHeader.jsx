104731494490dcdb4f143faa9e7aa9742ed1aa02
gift56 <efeasiughu@gmail.com>
2022-10-25 14:52:04 +0100

    added buton

--- src/components/AccountData/AcountHeader.jsx
+++ src/components/AccountData/AcountHeader.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../../assets/logo.png";
 import cart from "..././assets/cart.png";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 
 const AcountHeader = () => {
   const [show, setShow] = useState(false);
