ae61c579d1accaba0282479609435ca29cbbdcb5
gift56 <efeasiughu@gmail.com>
2022-10-25 12:11:44 +0100

    working on some stuff

--- src/components/FormData/UseForm.js
+++ src/components/FormData/UseForm.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Validate } from "./Validate";
+import { Validate } from "./Validation";
+
 
 const Useform = () => {
   const navigate = useNavigate();
