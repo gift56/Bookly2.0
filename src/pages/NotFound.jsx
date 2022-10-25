9c34e9efae945ed8226f2a2b704576b3a3ec7088
gift56 <efeasiughu@gmail.com>
2022-10-25 10:17:38 +0100

    added some stuff

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BiError } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 const NotFound = () => {
   return (
