480b50b5bdf53a05ec8c434d682afe6a54f9ec7c
gift56 <efeasiughu@gmail.com>
2022-10-26 13:52:05 +0100

    working some stuff

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AccountHero from "../components/AccountData/AccountHero";
 import AcountHeader from "../components/AccountData/AcountHeader";
 
 const AccountPage = () => {
   return (
     <>
       <AcountHeader />
+      <AccountHero />
     </>
   );
 };
