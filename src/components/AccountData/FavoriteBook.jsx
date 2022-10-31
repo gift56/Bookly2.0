bbdc71992eee58197d6ea6dd33cf542ef6bc0332
gift56 <efeasiughu@gmail.com>
2022-10-31 17:18:19 +0100

    working on importing some stuff

--- src/components/AccountData/FavoriteBook.jsx
+++ src/components/AccountData/FavoriteBook.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const FavoriteBook = () => {
+  const books = [];
   return (
     <section className="favoriteCon">
       <div className="contain">
         <div className="favoriteArea">
-            <div className="favoriteLeftside"></div>
-            <div className="favoriteRightside"></div>
+          <div className="favoriteLeftside"></div>
+          <div className="favoriteRightside"></div>
         </div>
       </div>
     </section>
