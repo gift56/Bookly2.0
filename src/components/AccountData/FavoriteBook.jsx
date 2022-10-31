12d713e1751f27065a472ca557f1cf193bd3eed7
gift56 <efeasiughu@gmail.com>
2022-10-31 17:22:55 +0100

    mapping through array

--- src/components/AccountData/FavoriteBook.jsx
+++ src/components/AccountData/FavoriteBook.jsx
@@ -3,14 +3,18 @@ import governBook from "../../assets/governB.png";
 import historyBook from "../../assets/historyB.png";
 
 const FavoriteBook = () => {
-  const booksImages = [governBook, historyBook, governBook, historyBook];
-  
+  const bookImages = [governBook, historyBook, governBook, historyBook];
+
   return (
     <section className="favoriteCon">
       <div className="contain">
         <div className="favoriteArea">
           <div className="favoriteLeftside">
-            {}
+            {bookImages.map((image,index)=>(
+                <div className="image">
+                    <img src={image} alt="book_image" />
+                </div>
+            ))}
           </div>
           <div className="favoriteRightside"></div>
         </div>
