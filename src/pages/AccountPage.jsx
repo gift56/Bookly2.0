c90978a69ce28e4892756369cb11ef2c16cb60bb
gift56 <efeasiughu@gmail.com>
2022-10-31 15:38:12 +0100

    done with herro text

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,11 +1,13 @@
 import AccountHero from "../components/AccountData/AccountHero";
 import AcountHeader from "../components/AccountData/AcountHeader";
+import Features from "../components/Home/Features";
 
 const AccountPage = () => {
   return (
     <>
       <AcountHeader />
       <AccountHero />
+      <Features rowId="1" title="Top Features" />
     </>
   );
 };
