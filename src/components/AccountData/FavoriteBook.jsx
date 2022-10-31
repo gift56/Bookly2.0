d420463192644cc1536bb29f9b92c0e1481c2c98
gift56 <efeasiughu@gmail.com>
2022-10-31 17:20:25 +0100

    created array of images

--- src/components/AccountData/FavoriteBook.jsx
+++ src/components/AccountData/FavoriteBook.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import governBook from "../../assets/governB.png";
 import historyBook from "../../assets/historyB.png";
 
-
 const FavoriteBook = () => {
-  const books = [];
+  const booksImages = [governBook, historyBook, governBook, historyBook];
   return (
     <section className="favoriteCon">
       <div className="contain">
