6eac5519e6b43b483ae27482762824c4681652cf
Efe G <98314362+gift56@users.noreply.github.com>
2022-12-20 08:14:31 -0800

    done working on this page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header/Header";
 
 const NotFound = () => {
+  
   return (
     <div>
       <Header btnText="login" linkText="login" />
